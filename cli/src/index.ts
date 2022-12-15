3d74ca231ce225a9bf75d25b9dd9935b45c0db12
gemsvidø <gemsvido@gmail.com>
2022-12-15 21:14:47 +0000

    move

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,8 +1,6 @@
 import args from "./lib/args.js";
 import { downloadPackage } from "./packages/download.js";
 
-for (const item of Object.entries(args)) {
-    if (args.task === "i" && args["hello-world"] == true) {
-        downloadPackage("hello-world");
-    }
+if (args.task === "i" && args["hello-world"] == true) {
+    downloadPackage("hello-world");
 }
